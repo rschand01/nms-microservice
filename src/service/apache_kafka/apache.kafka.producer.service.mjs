c5d0dd5b4caed9c4e73be85350150e2802e6df07
Reginald Chand <rschand01@gmail.com>
2024-08-09 00:32:46 +1200

    Update code quality, add error handling & paramter checks. Add JSDOC

--- src/service/apache_kafka/apache.kafka.producer.service.mjs
+++ src/service/apache_kafka/apache.kafka.producer.service.mjs
@@ -1,7 +1,28 @@
 import { kafkaProducer } from "./apache.kafka.service.mjs";
 import { logger } from "../../config/logger.config.mjs";
+import { requiredParamsUtility } from "../../utility/required.params.utility.mjs";
+import { typeCheckerUtility } from "../../utility/type.checker.utility.mjs";
 
-export const kafkaProduce = async (topic, messages = []) => {
+/**
+ * @description Function to send messages to the Kafka Broker with the assigned topic and message(s).
+ * @param {string} topic The Kafka topic to publish messages to.
+ * @param {Array<object>} messages An array of message objects to be sent to the topic.
+ */
+export const kafkaProduce = async (
+  topic = requiredParamsUtility("topic"),
+  messages = requiredParamsUtility("messages")
+) => {
+  typeCheckerUtility(topic, "string");
+
+  if (
+    !Array.isArray(messages) ||
+    !messages.every(
+      (message) => typeof message === "object" && message !== null
+    )
+  ) {
+    throw new TypeError("messages must be an array of objects!");
+  }
+
   try {
     await kafkaProducer.send({ topic: topic, messages: messages });
 
