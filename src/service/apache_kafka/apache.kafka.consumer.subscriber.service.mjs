6d57ebb794587a3ba45b0c2db25454b678426163
Reginald Chand <rschand01@gmail.com>
2024-08-09 00:49:11 +1200

    Update code quality, add type checks & paramter checks. Add JSDOC

--- src/service/apache_kafka/apache.kafka.consumer.subscriber.service.mjs
+++ src/service/apache_kafka/apache.kafka.consumer.subscriber.service.mjs
@@ -1,7 +1,21 @@
 import { kafkaConsumer } from "./apache.kafka.service.mjs";
 import { logger } from "../../config/logger.config.mjs";
+import { requiredParamsUtility } from "../../utility/required.params.utility.mjs";
+import { typeCheckerUtility } from "../../utility/type.checker.utility.mjs";
 
-export const kafkaConsumerSubscribe = async (topic, fromBeginning) => {
+/**
+ * @description Function to subscribe to a Kafka topic.
+ * @param {string} topic The Kafka topic to subscribe to.
+ * @param {boolean} fromBeginning Whether to start reading from the beginning of the topic.
+ * @throws {TypeError} Throws if `topic` is not a string or `fromBeginning` is not a boolean.
+ */
+export const kafkaConsumerSubscribe = async (
+  topic = requiredParamsUtility("topic"),
+  fromBeginning = requiredParamsUtility("fromBeginning")
+) => {
+  typeCheckerUtility(topic, "string");
+  typeCheckerUtility(fromBeginning, "boolean");
+
   try {
     await kafkaConsumer.subscribe({
       topic: topic,
