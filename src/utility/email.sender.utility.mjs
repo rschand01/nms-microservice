9d433d75286ff0d50d3c78b62faa951245bb84ae
Reginald Chand <rschand01@gmail.com>
2024-08-09 20:50:48 +1200

    Create email.sender.utility.mjs

---
+++ src/utility/email.sender.utility.mjs
@@ -0,0 +1,23 @@
+import { logger } from "../config/logger.config.mjs";
+import { requiredParamsUtility } from "./required.params.utility.mjs";
+import { typeCheckerUtility } from "./type.checker.utility.mjs";
+
+export const emailSenderUtility = (
+  notificationData = requiredParamsUtility("notificationData")
+) => {
+  typeCheckerUtility(notificationData, "object");
+
+  try {
+    logger.log({
+      level: "info",
+      message: notificationData.notificationMessage,
+      additional: notificationData.notificationTag,
+    });
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: error.message,
+      additional: error.stack,
+    });
+  }
+};
