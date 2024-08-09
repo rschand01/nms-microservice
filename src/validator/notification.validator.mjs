13d1a322e87c604d9701115539a47d8882044424
Reginald Chand <rschand01@gmail.com>
2024-08-09 17:49:02 +1200

    Add more fields

--- src/validator/notification.validator.mjs
+++ src/validator/notification.validator.mjs
@@ -2,10 +2,25 @@ import Joi from "joi";
 
 export const notificationValidator = Joi.object({
   notificationTopic: Joi.string().required(),
+
   notificationData: Joi.object({
     notificationType: Joi.string().required(),
     notificationTag: Joi.string().required(),
     notificationIdentifier: Joi.string().required(),
     notificationMessage: Joi.string().required(),
+
+    additionalNotificationData: Joi.object({
+      userFirstName: Joi.string().required(),
+      userLastName: Joi.string().required(),
+      userName: Joi.string().required(),
+      userEmailAddress: Joi.string().required(),
+      userPhoneContact: Joi.string().required(),
+
+      userCommunicationPreference: Joi.object({
+        emailNotification: Joi.boolean().required(),
+        smsNotification: Joi.boolean().required(),
+        pushNotification: Joi.boolean().required(),
+      }).required(),
+    }).required(),
   }).required(),
 });
