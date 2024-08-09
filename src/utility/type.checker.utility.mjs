33143bb1a44e2efe550387e3d0cf204783d9299b
Reginald Chand <rschand01@gmail.com>
2024-08-09 23:55:55 +1200

    Add JSDOC

--- src/utility/type.checker.utility.mjs
+++ src/utility/type.checker.utility.mjs
@@ -2,7 +2,7 @@ import { requiredParamsUtility } from "./required.params.utility.mjs";
 
 /**
  * @description Checks the type of a given element. Throws an error if the element is not of the expected type or if the parameters are invalid.
- * @param {string} element The value whose type is being checked. Must be a string.
+ * @param {any} element The value whose type is being checked.
  * @param {string} type The expected type of the element. Should be one of the allowed types.
  * @throws {TypeError} Throws an error if `element` is not a string, or if `type` is not in the allowed types, or if `element` is not of the expected type.
  */
