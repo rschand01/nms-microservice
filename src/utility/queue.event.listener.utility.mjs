52aca06d2002d550b84b5d70e05c15fb8e14941f
Reginald Chand <rschand01@gmail.com>
2024-08-09 23:49:03 +1200

    Add JSDOC, error handling

--- src/utility/queue.event.listener.utility.mjs
+++ src/utility/queue.event.listener.utility.mjs
@@ -2,95 +2,109 @@ import { logger } from "../config/logger.config.mjs";
 import { requiredParamsUtility } from "./required.params.utility.mjs";
 import { typeCheckerUtility } from "./type.checker.utility.mjs";
 
+/**
+ * @description Sets up event listeners for a Bull queue to log various events, such as job status changes and errors.
+ *
+ * @param {Bull} queue The Bull queue instance to attach event listeners to.
+ * @param {string} queueName The name of the queue, used in log messages for context.
+ */
 export const queueEventListenerUtility = (
   queue = requiredParamsUtility("queue"),
   queueName = requiredParamsUtility("queueName")
 ) => {
   typeCheckerUtility(queueName, "string");
 
-  queue.on("active", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} is active!`,
+  try {
+    queue.on("active", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} is active!`,
+      });
     });
-  });
 
-  queue.on("completed", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} completed!`,
+    queue.on("completed", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} completed!`,
+      });
     });
-  });
 
-  queue.on("progress", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} in progress!`,
+    queue.on("progress", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} in progress!`,
+      });
     });
-  });
 
-  queue.on("waiting", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job} is waiting!`,
+    queue.on("waiting", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job} is waiting!`,
+      });
     });
-  });
 
-  queue.on("stalled", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} is stalled!`,
+    queue.on("stalled", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} is stalled!`,
+      });
     });
-  });
 
-  queue.on("failed", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} failed!`,
+    queue.on("failed", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} failed!`,
+      });
     });
-  });
 
-  queue.on("removed", (job) => {
-    logger.log({
-      level: "info",
-      message: `Job ${job.id} removed successfully!`,
+    queue.on("removed", (job) => {
+      logger.log({
+        level: "info",
+        message: `Job ${job.id} removed successfully!`,
+      });
     });
-  });
 
-  queue.on("paused", () => {
-    logger.log({
-      level: "info",
-      message: `The ${queueName} is paused!`,
+    queue.on("paused", () => {
+      logger.log({
+        level: "info",
+        message: `The ${queueName} is paused!`,
+      });
     });
-  });
 
-  queue.on("resumed", () => {
-    logger.log({
-      level: "info",
-      message: `The ${queueName} is resumed!`,
+    queue.on("resumed", () => {
+      logger.log({
+        level: "info",
+        message: `The ${queueName} is resumed!`,
+      });
     });
-  });
 
-  queue.on("cleaned", (oldJobs) => {
-    logger.log({
-      level: "info",
-      message: `Old Jobs: ${oldJobs}`,
+    queue.on("cleaned", (oldJobs) => {
+      logger.log({
+        level: "info",
+        message: `Old Jobs: ${oldJobs}`,
+      });
     });
-  });
 
-  queue.on("drained", () => {
-    logger.log({
-      level: "info",
-      message:
-        "All waiting jobs have been processed. There can be some delayed job not yet processed!",
+    queue.on("drained", () => {
+      logger.log({
+        level: "info",
+        message:
+          "All waiting jobs have been processed. There can be some delayed job not yet processed!",
+      });
     });
-  });
 
-  queue.on("error", (error) => {
+    queue.on("error", (error) => {
+      logger.log({
+        level: "error",
+        message: error.message,
+        additional: error.stack,
+      });
+    });
+  } catch (error) {
     logger.log({
       level: "error",
       message: error.message,
       additional: error.stack,
     });
-  });
+  }
 };
