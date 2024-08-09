d3ed60e536925ce905a71dcb2161199bdbd7091b
Reginald Chand <rschand01@gmail.com>
2024-08-09 20:48:51 +1200

    Add constants

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -1,2 +1,5 @@
 export const WINDOWS_MS = 60 * 60 * 1000;
 export const API_LIMIT = 100;
+export const OTP = "OTP";
+export const MAX_JOBS_TO_BE_PROCESSED = 5;
+export const MAX_JOBS_TO_BE_PROCESSED_DURATION_IN_SECONDS = 5;
