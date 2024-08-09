3ed24fc56a7b7660c121a1c4375a345d6e204f51
Reginald Chand <rschand01@gmail.com>
2024-08-10 00:45:51 +1200

    Create server.mjs

---
+++ server.mjs
@@ -0,0 +1,58 @@
+import cors from "cors";
+import { config as dotEnvConfig } from "dotenv";
+import { envValidator } from "./src/validator/env.validator.mjs";
+import express from "express";
+import { initializeKafka } from "./src/service/apache_kafka/apache.kafka.service.mjs";
+import { kafkaConsumerSubscribe } from "./src/service/apache_kafka/apache.kafka.consumer.subscriber.service.mjs";
+import { logger } from "./src/config/logger.config.mjs";
+import { rateLimitedQueueWorker } from "./src/worker/rate.limited.queue.worker.mjs";
+import { rateLimiter } from "./src/middleware/rate.limit.middleware.mjs";
+import { router } from "./src/router/router.mjs";
+
+const app = express();
+
+dotEnvConfig();
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+(async () => {
+  const { error } = envValidator.validate(process.env);
+
+  try {
+    if (error) {
+      logger.log({ level: "error", message: error.message });
+      process.exit(1);
+    }
+
+    await initializeKafka();
+    await kafkaConsumerSubscribe("transcational", true);
+    await kafkaConsumerSubscribe("promotional", true);
+
+    rateLimitedQueueWorker();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.use(cors(corsOptions));
+
+    app.use(rateLimiter);
+    app.use("/", router);
+
+    app.listen(process.env.EXPRESS_PORT, () => {
+      logger.log({
+        level: "info",
+        message: "Express Server is successfully listening!",
+        additional: `port: ${process.env.EXPRESS_PORT}`,
+      });
+    });
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: error.message,
+      additional: error.stack,
+    });
+  }
+})();
